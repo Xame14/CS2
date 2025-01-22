408e9c18394035f13938dcae2d9621abb0bcd63d
Xame14 <Arcedamian2006@gmail.com>
2025-01-22 11:57:48 -0600

    Create "Sheet of paper".cpp

---
+++ "Sheet of paper".cpp
@@ -0,0 +1,15 @@
+int array[5];
+array[3] = 20;
+array[1] = array[3];
+array[4] = array[3];
+array[0] = 15;
+
+cout<<"Please enter an integer for index 2:  ";
+cin>>array[2];
+
+int total = 0;
+
+for(i = 0; i < 5; i++){
+  total = total + array[i];
+  }
+cout<<total;
