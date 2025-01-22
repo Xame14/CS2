0b03ed23b3a282c2f93c3bcd1dd1388fa113f07a
Xame14 <Arcedamian2006@gmail.com>
2025-01-22 12:01:54 -0600

    Update "Sheet of paper".cpp

--- "Sheet of paper".cpp
+++ "Sheet of paper".cpp
@@ -1,19 +1,22 @@
 #include <iostream> 
 using namespace std;
 
-int main()
-int array[5];
-array[3] = 20;
-array[1] = array[3];
-array[4] = array[3];
-array[0] = 15;
+int main() {
+  int array[5];
+  array[3] = 20;
+  array[1] = array[3];
+  array[4] = array[3];
+  array[0] = 15;
 
-cout<<"Please enter an integer for index 2:  ";
-cin>>array[2];
+  cout<<"Please enter an integer for index 2:  ";
+  cin>>array[2];
 
-int total = 0;
+  int total = 0;
 
-for(i = 0; i < 5; i++){
-  total = total + array[i];
-  }
-cout<<total;
+  for(i = 0; i < 5; i++){
+    total = total + array[i];
+    }
+  
+  cout<<total;
+return 0;
+}
