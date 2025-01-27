6f36a17abd91a2855524e95633d0a0527ed9127f
Xame14 <Arcedamian2006@gmail.com>
2025-01-27 11:06:21 -0600

    Create 1darrays.cpp

---
+++ 1darrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ar[10];
+    
+    int br[5] = {10, 20, 30, 40, 50}; // initializer list
+    
+    int cr[6] = {10,20,30};
+    
+    int dr[] = 10, 20, 30};
+}
