9f22ba47cb0a1e20259004681810a22c4d7480a6
Xame14 <Arcedamian2006@gmail.com>
2025-01-29 13:52:45 -0600

    Update lab 2.cpp

--- lab 2.cpp
+++ lab 2.cpp
@@ -5,30 +5,67 @@ using namespace std;
 
 int difference(int,int);
 void input(int&, int&);
-//findprimenumbers();
+void findprimenumbers(int [],int);
+bool checkprime(int);
 
 int main()
 {
-    cout<<difference(20, 3);
-    cout<<difference(-4, 13);
+    cout<<difference(20, 3)<<endl;
+    cout<<difference(-4, 13)<<endl;
     
     int n1,n2;
     input(n1,n2);
     
-    cout<<n1<<endl;
-    cout<<n2<<endl;
+    cout<<"You have entered: "<<n1<<endl;
+    cout<<"You have entered: "<<n2<<endl;
+    
+    int A[5] = {2, 11, 33, 41, 10};
+    findprimenumbers(A,5);
 
     return 0;
 }
 
 int difference(int a,int b){
+    int c;
+    if(a < 0){
+        b = b *-1;
+    }
+    if(b < 0){
+        a = a *-1;
+    }
     c = a - b;
     return c;
     
 }
 
 void input(int &a,int &b){
-    int a,b;
     cout<<"Enter two intergers: ";
     cin >> a >> b;
 }
+
+void findprimenumbers(int arr[], int total){
+    for(int i = 0; i < total;i++){
+        bool ans = checkprime(arr[i]);
+        if(ans == true){
+            cout<<arr[i]<<" is a prime number."<<endl;
+            
+            }
+            
+    
+    }
+}
+
+bool checkprime(int a){
+    for(int i = 2; i <= a - 1;i++){
+        if(a % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+
+
