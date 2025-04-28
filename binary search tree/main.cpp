a16ca136957931195805674eb469db14af5724c5
Xame14 <Arcedamian2006@gmail.com>
2025-04-28 11:25:55 -0600

    Create main.cpp

---
+++ binary search tree/main.cpp
@@ -0,0 +1,22 @@
+#include "bst.h"
+
+
+int main()
+{
+    BST b;
+    cout<<b.size()<<endl; //0
+    cout<<b.height()<<endl; // -1
+    
+    b.insert(20);
+    b.insert(5);
+    b.insert(10);
+    b.insert(25);
+    b.insert(31);
+    b.insert(33);
+    
+    cout<<b.size()<<endl; //7
+    cout<<b.height()<<endl; // 3
+    
+
+    return 0;
+}
