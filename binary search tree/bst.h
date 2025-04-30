854c330f898870e140785ac46206682e3af3b7d4
Xame14 <Arcedamian2006@gmail.com>
2025-04-30 11:25:22 -0600

    Update bst.h

--- binary search tree/bst.h
+++ binary search tree/bst.h
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
+// in a balanced BST, height = log_2 n (log n based 2)
 class BST{
   public:
-  BST();
+  BST(); 
   ~BST();
-  int size();
-  int height();
-  void insert(int value);
-  bool search(int value); //depth-first
+  int size();                                       //O(n)
+  int height();                                     //O(n)
+  void insert(int value);                           //O(height)
+  bool search(int value); //depth-first             //O(height)
   void inorderTraversal(); //left, parent, right
   void preorderTraversal(); //parent, left, right
   void postorderTraversal(); //left, right, parent
@@ -26,5 +26,9 @@ class BST{
   void deleteNodes(Node* p);
   int sizeR(Node* p); // size of the tree with parentnp
   int heightR(Node* p); //height of the tree with parent p
-  void insertR(Node* &p, int value); // insert value in the tree 
+  void insertR(Node* &p, int value); // insert value in the tree
+  bool searchR(Node* p, int value); //returns true or false if
+  void inorderTraversalR(Node* p); //left, parent, right
+  void preorderTraversalR(Node* p); //parent, left, right
+  void postorderTraversalR(Node* p);//left, right, parent
 };
