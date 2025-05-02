7ab596655fc3c62739a90c986d402f627ca72575
Xame14 <Arcedamian2006@gmail.com>
2025-05-02 12:06:12 -0600

    Update linkedList.h

--- final project/linked list/linkedList.h
+++ final project/linked list/linkedList.h
@@ -1,18 +1,22 @@
-class linkedList{
-  public:
-  linkedList();
-  ~linkedList();
-  int size();   // returns size
-  void print();      // prints
-  void push(int);    // will insert value to the front
-  int pop();        // pop smallest value
-  
-  private:
-  class Node{
-    int data;
-    Node* next;
-  };
-  Node* head;
-  int count;
-    
+#include <iostream>
+using namespace std;
+
+class linkedList {
+public:
+	linkedList();
+	~linkedList();
+	int size();   // returns size
+	void print();      // prints
+	void push(int);    // will insert value to the front
+	int pop();        // pop smallest value
+	class Node {
+	    public:
+		int data;
+		Node* next;
+	};
+	
+	private:
+	Node* head;
+	int count;
+
 };
