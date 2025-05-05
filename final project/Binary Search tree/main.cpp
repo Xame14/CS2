e012e5f0f7c4b2675ddd90c0178a31d36ced30f7
Xame14 <Arcedamian2006@gmail.com>
2025-05-05 12:51:14 -0600

    Update main.cpp

--- final project/Binary Search tree/main.cpp
+++ final project/Binary Search tree/main.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include "Autocompleter.h"
+using namespace std;
 
+int main(){
+    
+    Autocompleter Name;
+    
+    Name.insert("Raymond");
+    Name.insert("Noah");
+    Name.insert("Felipe");
+    Name.insert("Lawrence");
+    
+    cout<<"The current size is: "<< Name.size()<<endl;
+    cout<<"The hight is currently: "<< Name.height()<<endl;
+    
+    Name.insert("Benjamin");
+    Name.insert("Joseph");
+    Name.insert("Hiram");
+    Name.insert("Arnold");
+    
+    cout<<"The current size is: "<< Name.size()<<endl;
+    cout<<"The hight is currently: "<< Name.height()<<endl;
+    
+    Name.insert("Boe");
+    Name.insert("Sonya");
+    Name.insert("Neville");
+    
+    cout<<"The current size is: "<< Name.size()<<endl;
+    cout<<"The hight is currently: "<< Name.height()<<endl;
+    
+    
+    
+    
+    return 0;
+}
