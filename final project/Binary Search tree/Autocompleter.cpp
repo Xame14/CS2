5c50f0c92d6753d6c19f85e4c0c30b6fd49abbb5
Xame14 <Arcedamian2006@gmail.com>
2025-05-07 23:11:02 -0600

    Update Autocompleter.cpp

--- final project/Binary Search tree/Autocompleter.cpp
+++ final project/Binary Search tree/Autocompleter.cpp
@@ -68,7 +68,28 @@ void Autocompleter::insertR(Node* &p,string word)//inserts the string value in t
 
 }
 
-void Autocompleter::completion(string letter)
+void Autocompleter::completionR(Node* p,char letter)
 {
-       
+    if(p == nullptr)
+        return;
+    string word = p->name;
+    char first_letter = word[0];
+    
+    if(letter >= 97)
+        letter = letter - 32;
+    
+    if(first_letter == letter){
+        cout<< p->name << " ";
+        return completionR(p->right, letter);
+    }
+    else if(first_letter > letter)
+        return completionR(p->left, letter);
+    else
+        return completionR(p->right, letter);
 }
+
+void Autocompleter::completion(char letter)
+{
+    return completionR(root,letter);
+}
+
